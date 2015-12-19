7a0300a699cb7ec962c3ed5d724788b42089a027
Brovko Roman <brovko-roman@ya.ru>
2015-12-19 23:34:49 +0200

    lesson 18 - UIWebView and SFSafariViewController - end project

---
+++ Restaraunts/WebViewController.swift
@@ -0,0 +1,42 @@
+//
+//  WebViewController.swift
+//  Restaraunts
+//
+//  Created by Игорь on 14.12.15.
+//  Copyright © 2015 Ihor Malovanyi. All rights reserved.
+//
+
+import UIKit
+
+
+class WebViewController: UIViewController {
+
+    @IBOutlet weak var webView: UIWebView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        
+        if let url = NSURL(string: "http://apple.com") {
+            let request = NSURLRequest(URL: url)
+            webView.loadRequest(request)
+        }
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
